d106da73863c100bc0f4e68883d0dc3079e6d465
ShimenAfshar <afsharshimen@gmail.com>
2022-11-22 11:22:45 +0000

    factorial and get average done

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,11 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+    let newArray = numbers.filter((i) => typeof i === "number");
+    let totalValue = newArray.reduce((a, b) => a + b);
+    let average = totalValue / newArray.length;
+    return average;
+}
 
-module.exports = average;
+module.odule.exports = average;
--- A-pass-tests/09-car-sales/car-sales.js
+++
@@ -1,3 +0,0 @@
-function sales(carsSold) {}
-
-module.exports = sales;
